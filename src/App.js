65787f8f991d8e4db6eab56199cb6096feed6f68
SANGJUN <sjun8932@gmail.com>
2021-08-30 12:25:48 +0900

    2. props 연습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const information = {
+    title: '게시글 제목입니다.',
+    content: '게시글 내용입니다. 반갑습니다.',
+    user: {
+        name: '박상준',
+        ImgUrl: 'https://w.namu.la/s/dfa30564fb91f6b91c21419685e4c786d72dbe1e0af5f7cbaa0c712e548251a53cf19e909ac88ad424c9f4ac1d518012c4c4fcf60be649d1580443fb722fb886df56f07151f0e8e922423b38f72331fbc7eea7a6793539b1362e378eb722f775d4851463a070491e4fd12927ea8357c1'
+    }
+}
+
+function User(props) {
     return (
-        <div className="gray">
-            <img src={logo} alt="logo" />
+        <div>
+            <img src={props.user.ImgUrl} alt="image"/>
+            <br/>
+            <strong>{props.user.name}</strong>
         </div>
     );
 }
 
-export default App;
+function Board(props){
+    return(
+        <section>
+            <User user={props.user}/>
+            {props.title}
+            <hr/>
+            {props.content}
+        </section>
+    );
+}
+
+
+
+
+export {information};
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Board from './App'
+import {information} from './App'
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Board
+        title={information.title}
+        content={information.content}
+        user={information.user}
+    />,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
