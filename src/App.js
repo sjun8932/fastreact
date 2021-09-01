843dc746b90e94ccfe71f9410009e8b73454c9f2
SANGJUN <sjun8932@gmail.com>
2021-09-01 12:50:48 +0900

    4. Material UI & WithStyles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
+import React, {Component} from "react";
+import Customer from "./components/Customer";
 import './App.css';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: "auto"
+    },
+    table: {
+        minWidth: 1080
+    }
+})
+
+const customers =[
+    {
+        'id' : '1',
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : '곽민선',
+        'birthday' : '920304',
+        'gender' : 'Women',
+        'job' : '아나운서',
+    },
+    {
+        'id' : '2',
+        'image' : 'https://placeimg.com/64/64/2',
+        'name' : '이수날',
+        'birthday' : '930403',
+        'gender' : 'Women',
+        'job' : '유튜버',
+    },
+    {
+        'id' : '3',
+        'image' : 'https://placeimg.com/64/64/3',
+        'name' : '서현숙',
+        'birthday' : '940422',
+        'gender' : 'Women',
+        'job' : '치어리더',
+    }
+]
 
 
 
-function User(props) {
-    return (
-        <div>
-            <img src={props.user.ImgUrl} alt="profile"/>
-            <br/>
-            <strong>{props.user.name}</strong>
-        </div>
-    );
+class App extends Component {
+
+    render(){
+        const { classes } = this.props;
+        return(
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {customers.map(c=>{ return (<Customer key={c.name} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
 
-function Board(props){
-    return(
-        <section>
-            <User user={props.user}/>
-            {props.title}
-            <hr/>
-            {props.content}
-        </section>
-    );
-}
 
-export default Board;
+
+export default withStyles(styles)(App);
+
+
+
