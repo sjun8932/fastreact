366be94b5d2a8fec0c48273e8e5de582c008f9eb
SANGJUN <sjun8932@gmail.com>
2021-09-03 13:56:37 +0900

    6. 고객 DB 테이블 구축 및 express와 연동

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +8,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port:conf.port,
+    database: conf.database,
+});
+connection.connect();
+
 app.get('/api/customers',(req,res) => {
-    res.send([
-        {
-            'id' : '1',
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '곽민선',
-            'birthday' : '920304',
-            'gender' : 'Women',
-            'job' : '아나운서',
-        },
-        {
-            'id' : '2',
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '이수날',
-            'birthday' : '930403',
-            'gender' : 'Women',
-            'job' : '유튜버',
-        },
-        {
-            'id' : '3',
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '서현숙',
-            'birthday' : '940422',
-            'gender' : 'Women',
-            'job' : '치어리더',
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, ()=> console.log(`${port}번 서버가 작동중 입니다~ Happy~~`))
