6cfcb5654dddd4412765cfd9e344389f2336fc94
SANGJUN <sjun8932@gmail.com>
2021-09-03 16:33:30 +0900

    8. Express에서 파일 업로드 요청 처리 및 DB에 데이터 삽입

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 app.get('/api/customers',(req,res) => {
     connection.query(
         "SELECT * FROM CUSTOMER",
@@ -29,5 +32,22 @@ app.get('/api/customers',(req,res) => {
         }
     );
 });
+
+app.use('./image',express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req,res)=>{
+    let sql = 'INSERT INTO CUSTOMER VALUES (null,?,?,?,?,?)';
+    let image =  '/image/' + req.file.fileName;
+    let name = req.body.name;
+    let birthday= req.body.birthday;
+    let gender =  req.body.gender;
+    let job =  req.body.job;
+    let params = [image, name, birthday, gender, job,]
+
+    connection.query(sql, params, (err, rows, fileds) => {
+        res.send(rows);
+        }
+    );
+})
 
 app.listen(port, ()=> console.log(`${port}번 서버가 작동중 입니다~ Happy~~`))
