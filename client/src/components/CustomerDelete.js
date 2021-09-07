442a9b08fc4ed087b86ed52540d2db9220d27d84
SANGJUN <sjun8932@gmail.com>
2021-09-07 10:33:08 +0900

    10. 고객(Customer) 정보 삭제 기능 구현하기

---
+++ client/src/components/CustomerDelete.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class CustomerDelete extends  Component {
+
+    deleteCustomer(id){
+        const url = '/api/Customers/' + id;
+        fetch(url,{
+            method: 'DELETE'
+        });
+        this.props.stateRefresh();
+    }
+
+
+    render(){
+        return (
+            <button onClick={(e)=>{this.deleteCustomer(this.props.id)}}>삭제</button>
+        );
+    }
+}
+
+export default CustomerDelete;
